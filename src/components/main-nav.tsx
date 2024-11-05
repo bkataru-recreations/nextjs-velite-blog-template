ab264f6cf6cc019721f23afbcc266883610ebe36
Baalateja Kataru <baalateja.k@gmail.com>
2024-11-05 09:47:47 +0000

    on the road to stage 3

---
+++ src/components/main-nav.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import Link from "next/link";
+import { Icons } from "./icons";
+import { siteConfig } from "@/config/site";
+
+export function MainNav() {
+	return (
+		<nav className="flex items-center space-x-4 lg:space-x-6">
+			<Link href="/" className="mr-6 flex items-center space-x-2">
+				<Icons.logo className="h-6 w-6" />
+				<span className="font-bold">{siteConfig.name}</span>
+			</Link>
+		</nav>
+	);
+}
---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+	name: "JollyBlog",
+	url: "https://example.com",
+	description: "Nextjs 14 blog using velite, tailwind, and shadcn/ui",
+	author: "bkataru",
+	links: {
+		twitter: "https://x.com/baalatejakataru",
+		github: "https://github.com/bkataru",
+		personal: "https://github.com/bkataru",
+	},
+};
+
+export type SiteCnofig = typeof siteConfig;
